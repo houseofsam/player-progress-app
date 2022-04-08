4d6810f7afb4d29691fe85a63cc0f0590ee80b64
houseofsam <samxsaid@gmail.com>
2022-04-07 22:50:33 -0400

    Import Form component into App.js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
+import { Form } from "./components/Form";
+
 function App() {
   return (
-    <div className="container">
-      <h1>Player Progress App</h1>
-    </div>
+    <>
+      <div className="container">
+        <h1>Player Progress App</h1>
+        <Form />
+      </div>
+    </>
   );
 }
 
