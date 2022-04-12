ccb498ff9faee88190542aaf83bb1526410e53b4
houseofsam <samxsaid@gmail.com>
2022-04-11 22:39:37 -0400

    Add backend files to project

---
+++ express-server/db/seeds/01_users.sql
@@ -0,0 +1,2 @@
+INSERT INTO users (name, email, password) VALUES ('Test Test', 'test@mail.com', 'password');
+INSERT INTO users (name, email, password) VALUES ('Alice Watkins', 'alice@mail.com', 'password');
